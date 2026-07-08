cceeb1f7116acc55e996755adb0efcb0b63d1ee9
musinga33 <musinga48@gmail.com>
2026-07-08 20:49:23 +0200

    this it for learning Java programming from zero to hero

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String name = "Musinga";
+        int age = 24;
+        double gpa = 3.5;
+        char grade = 'A';
+        boolean isGraduated = true;
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Graduated: " + isGraduated);
+    }
+}
