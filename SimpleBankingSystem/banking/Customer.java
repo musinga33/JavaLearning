48f30750cbf5631e631625f98ecb2bb4ff2388c1
musinga33 <musinga48@gmail.com>
2026-07-26 21:34:39 +0200

    Exception in java with small banking system

---
+++ SimpleBankingSystem/banking/Customer.java
@@ -0,0 +1,47 @@
+package SimpleBankingSystem.banking;
+
+public class Customer {
+    private String customerId;
+    private String name;
+    private String phoneNumber;
+    private Account account;
+
+    // constructor
+
+    public Customer(String customerId, String name, String phoneNumber, Account account) {
+        this.customerId = customerId;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.account = account;
+    }
+
+    // Display customer information
+
+    public void displayCustomerInfo() {
+
+        System.out.println("Customer ID:" + customerId);
+        System.out.println("Name:" + name);
+        System.out.println("Phone Number:" + phoneNumber);
+
+        System.out.println("\nAccount Information:");
+    }
+
+    // getters
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public String getName() {
+        return name;
+
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+}
